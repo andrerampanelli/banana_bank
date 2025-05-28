957a113d234ea945f3209bf17263740a27289a0d
André Vitor Rampanelli <andrerampanelli1@gmail.com>
2025-05-28 20:33:36 +0300

    feat(users): add password field and hash generation to User changeset

--- lib/banana_bank/users/user.ex
+++ lib/banana_bank/users/user.ex
@@ -5,6 +5,7 @@ defmodule BananaBank.Users.User do
   schema "users" do
     field :name, :string
     field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :balance, :string, default: "000"
     field :address, :string
@@ -20,5 +21,12 @@ defmodule BananaBank.Users.User do
     |> validate_format(:name, ~r/^[A-ZÀ-Ýa-zà-ÿ\s]+$/)
     |> validate_length(:name, min: 1, max: 100)
     |> validate_length(:email, min: 5, max: 100)
+    |> add_password_hash()
   end
+
+  defp add_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, password_hash: Argon2.hash_pwd_salt(password))
+  end
+
+  defp add_password_hash(changeset), do: changeset
 end
