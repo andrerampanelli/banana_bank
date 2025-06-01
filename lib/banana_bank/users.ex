c7c9c7a520eb243374e5493e816e67816345f857
André Vitor Rampanelli <andrerampanelli1@gmail.com>
2025-06-01 20:02:44 +0300

    feat(users): add delete functionality for user accounts

--- lib/banana_bank/users.ex
+++ lib/banana_bank/users.ex
@@ -3,9 +3,10 @@ defmodule BananaBank.Users do
   Context module for user operations.
   """
 
-  alias BananaBank.Users.{Create, Get, Update}
+  alias BananaBank.Users.{Create, Get, Update, Delete}
 
   defdelegate create(params), to: Create, as: :call
   defdelegate get(id), to: Get, as: :call
   defdelegate update(id, params), to: Update, as: :call
+  defdelegate delete(id), to: Delete, as: :call
 end
