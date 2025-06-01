2313cbf97aa8789141d31819530053f653d1799b
André Vitor Rampanelli <andrerampanelli1@gmail.com>
2025-06-01 19:32:48 +0300

    feat(errors): add custom error handling for not found cases

--- lib/banana_bank_web/controllers/error_json.ex
+++ lib/banana_bank_web/controllers/error_json.ex
@@ -19,6 +19,10 @@ defmodule BananaBankWeb.ErrorJSON do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
 
+  def error(%{status: :not_found}) do
+    %{errors: "Not found"}
+  end
+
   def error(%{changeset: changeset}) do
     %{errors: Ecto.Changeset.traverse_errors(changeset, &translate_error/1)}
   end
